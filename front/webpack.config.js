53b1287257e3cd6fa42342aa210689b0fe8fec20
Dragon-taro <dragon.taro.lioil@gmail.com>
2019-03-17 16:02:52 +0900

    init

---
+++ front/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: ["@babel/polyfill", "./src/pages/index.tsx"],
+    admin: ["@babel/polyfill", "./src/pages/admin.tsx"]
+  },
+  output: {
+    path: path.join(__dirname, "src/bff/static"),
+    filename: "[name].bundle.js"
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "awesome-typescript-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader?modules"]
+      }
+    ]
+  },
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  }
+};
