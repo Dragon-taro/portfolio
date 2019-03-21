40507bd0477d23ee8a8c1e38b9598cb9eca030bb
Dragon-taro <dragon.taro.lioil@gmail.com>
2019-03-21 17:37:12 +0900

    コンフリクト解決

--- front/src/apps/index.tsx
+++ front/src/apps/index.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import TopContent from "../components/blocks/TopContent";
+import Contact from "../pages/Contact";
+import Footer from "../components/blocks/Footer";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../redux/rootReducer";
+
+const store = createStore(rootReducer);
 
 const App: React.FunctionComponent = () => (
-  <div>
-    <TopContent />
-  </div>
+  <Provider store={store}>
+    <React.Fragment>
+      <Contact />
+      <Footer />
+    </React.Fragment>
+  </Provider>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- front/src/pages/Contact/index.tsx
+++ front/src/pages/Contact/index.tsx
@@ -1,6 +1,36 @@
 import * as React from "react";
 import * as styles from "./style.css";
+import { RootState } from "../../types/root";
+import { IContact, ISetContact } from "../../types/contact";
+import { Dispatch } from "redux";
+import { Action } from "../../types/utils";
+import { setContact } from "../../redux/contact/actions";
+import { connect } from "react-redux";
 
-const Contact: React.FunctionComponent = () => <div>contact</div>;
+interface Props {
+  contact: IContact;
+  actions?: {
+    setContact: (dispatch: Dispatch<Action<ISetContact>>) => void;
+  };
+}
 
-export default Contact;
+const mapStateToProps = (state: RootState) => ({
+  contact: state.contact
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<Action<ISetContact>>) => ({
+  setContact: (payload: ISetContact) => {
+    dispatch(setContact(payload));
+  }
+});
+
+class Contact extends React.Component<Props> {
+  render() {
+    return <div />;
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Contact);
